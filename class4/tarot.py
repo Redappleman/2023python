30b56ec7b1538d692b70327992fa1a45dceaebb6
Redappleman <asdfqd@naver.com>
2023-04-11 14:08:20 +0900

    s

--- class4/tarot.py
+++ class4/tarot.py
@@ -1,5 +1,5 @@
 import random as r
-tarot = ['Fool','magician','temperence','lovers','the high priestess'
+tarot = ['Fool','the magician','temperence','lovers','the high priestess'
          ,'chariot','hanged man','death','wheel of fortune',
          'the sun','justice','empress','emperor','the tower',
          'the star','strength','the devil','the moon','judgement'
@@ -10,4 +10,4 @@ print(tarot)
 
 for k in range(1,5):
     index = r.randrange(22)
-    print('%d번째 타로는 %s'%(k+1,tarot[index]))
+    print('%d번째 타로는 %s'%(k,tarot[index]))
